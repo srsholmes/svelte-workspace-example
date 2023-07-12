b09ac11dcec0a3b919ff5b03a5d249189338c0b7
Simon Holmes <srsholmes@gmail.com>
2023-07-12 16:52:22 +0100

    first commit

---
+++ app/my-app/src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { addTwo, helloWorld } from '@package/my-lib/src/hello-world';
+
+  let n = addTwo(1);
+
+   let str =  helloWorld('Matías');
+
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>{str}</p>
+<p>{n}</p>
---
+++ package/my-lib/src/hello-world.ts
@@ -0,0 +1,5 @@
+export const helloWorld = (name: string) => {
+  console.log(`Hello ${name}`);
+  return `Hello ${name}`;
+};
+export const addTwo = (n: number) => n + 2;
